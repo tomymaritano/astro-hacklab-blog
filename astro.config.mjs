4dd138f7b7c882c3e4d1e9712aac1ef2e4bd047b
tomymaritano <tomymaritano@gmail.com>
2025-06-27 15:35:42 -0300

    feat: adding vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [react(), mdx()],
@@ -9,4 +10,10 @@ export default defineConfig({
       postcss: './postcss.config.js',
     },
   },
+   output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // set to false when using @vercel/analytics@1.4.0
+    },
+  }),
 });
